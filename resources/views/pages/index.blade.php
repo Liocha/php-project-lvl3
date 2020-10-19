07b6d777cec555e22e65a69dfef127e5725ef49a
Alex <dev@liocha.ru>
2020-10-20 00:22:26 +0300

    first stap

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+<div class="container-fluid">
+    <h1>Hello world</h1>
+</div>
+
+    
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index']);
